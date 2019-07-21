e209089380dba03b997852385b515f0aeba12f37
Odohi David <odohi.david@gmail.com>
2019-07-21 23:47:53 +0100

    add more configuration for the toast

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ iziToast.settings({
   transitionIn: 'flipInX',
   transitionOut: 'flipOutX',
   position: "bottomLeft",
+  progressBar: false,
+  close: false,
   // onOpening: function(){
   //     console.log('callback abriu!');
   // },
@@ -22,19 +24,81 @@ const messages = [
   {
     location: "Lagos",
     image: 'img/lagos.png'
+  },
+  {
+    location: "Lagos",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Abuja",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Abu Dhabi",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Dubai",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Dallas",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Charlotte",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Toronto",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Detroit",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Kumasi",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Ibadan",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Fortaleza",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Cordoba",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Trinidad and Tobago",
+    image: 'img/lagos.png'
+  },
+  {
+    location: "Las Vegas",
+    image: 'img/lagos.png'
   }
 ]
 
 let count = 0;
 let lenght = messages.length;
+let period = ["hours", "minutes", "seconds"];
+
+function randomDate(startHour, endHour) {
+  var hour = startHour + Math.random() * (endHour - startHour) | 0;
+  return date;
+}
 
 setInterval(()=> {
   let i = messages[count];
   iziToast.show({
     title: `Someone from ${i.location}`,
-    message: 'Just joined the training!',
+    message: `Just joined the training.<br/><small style="margin-top: .5rem">${Math.floor(Math.random()*10)} minutes ago.</small>`,
     layout: 2,
-    // imageWidth: '200',
+    // imageWidth: '80',
     maxWidth: '300',
     image: i.image,
   });
